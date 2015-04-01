c3c7c28c02dec8f1db4b7b12850c4ef414b05876
Kalinga Bhusan Ray <mail.kalinga@gmail.com>
2015-04-02 00:36:56 +0530

    Initial UI

---
+++ germanDictionary/src/DictionaryUI.java
@@ -0,0 +1,47 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+
+
+public class DictionaryUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DictionaryUI frame = new DictionaryUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public DictionaryUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setToolTipText("Select or type the german word");
+		comboBox.setEditable(true);
+		contentPane.add(comboBox, BorderLayout.SOUTH);
+	}
+
+}
